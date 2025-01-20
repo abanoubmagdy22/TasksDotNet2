bdfc4736637e3bdccd48bc4fe484df1ba4152a3f
a.magdy <a.magdy@classera.com>
2025-01-20 22:57:52 +0200

    new challenge

---
+++ ChallengesDay3/Program.cs
@@ -0,0 +1,86 @@
+﻿using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+namespace ChallengesDay3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //double temperature = 0;
+            //int windSpeed = 0;
+
+            //const int windSpeedForCoat = 25;      
+            //const double temperatureForCoat = 22; 
+
+            //Console.Write("Enter temperature: ");
+            //temperature = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Enter speed: ");
+            //windSpeed = Convert.ToInt32(Console.ReadLine());
+            //bool wearCoat = (temperature < temperatureForCoat) || (windSpeed > windSpeedForCoat);
+            //Console.WriteLine(wearCoat);
+
+            //double baseCost = 2.50;
+            //double surcharge = 0;
+
+            //Console.Write("Enter the length of shipping: ");
+            //int length = int.Parse(Console.ReadLine());
+
+            //Console.Write("Enter the width of shipping: ");
+            //int width = int.Parse(Console.ReadLine());
+
+            //Console.Write("Enter the height of shipping: ");
+            //int height = int.Parse(Console.ReadLine());
+
+            //int volumeOfShipping = length * width * height;
+            //if (volumeOfShipping > 500)
+            //{
+            //    surcharge += baseCost * 1.25;
+            //}
+            //else if (volumeOfShipping > 100)
+            //{
+            //    surcharge += baseCost * 1.1;
+            //}
+            //Console.WriteLine(surcharge);
+
+
+            int dayCode = 0;
+            Console.Write("Enter dayCode: ");
+            dayCode = Convert.ToInt32(Console.ReadLine());
+            switch (dayCode)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid day code");
+                    break;
+            }
+        }
+
+
+
+    }
+
+
+
+    }
+
---
+++ LoopSummition/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LoopSummition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            for (int i = 1; i <= 15; i++)
+            {
+                if (i % 2 != 0) // Check if the number is odd
+                {
+                    sum += i; // Add the odd number to the sum
+                }
+            }
+
+            Console.WriteLine("The sum of odd numbers from 1 to 15 is: " + sum);
+        }
+    }
+}
