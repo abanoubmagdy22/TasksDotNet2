027b669e0d453651fb3ea0a89652d3213a7d6f93
Abanoub Magdy <80099748+abanoubmagdy22@users.noreply.github.com>
2025-01-23 11:34:09 +0200

    update

--- SolveTaskDay3/Program.cs
+++ SolveTaskDay3/Program.cs
@@ -12,7 +12,7 @@
             do
             {
                 Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("\n Menu:");
+                Console.WriteLine("\n Menu Select your option:");
                 Console.WriteLine("=============================");
                 Console.ForegroundColor = ConsoleColor.Green;
                 // options of list 
@@ -46,6 +46,7 @@
                         }
                         else
                         {
+                            Console.ForegroundColor = ConsoleColor.White;
                             Console.Write("[");
 
                             for (int i = 0; i < numbers.Count; i++)
@@ -58,14 +59,19 @@
                             }
 
                             Console.WriteLine("]");
+                            Console.ForegroundColor = ConsoleColor.White;
+
                         }
                         break;
 
                     case 'A':
                         Console.Write("Enter an integer to add to the list: ");
-                        if (int.TryParse(Console.ReadLine(), out int number))
+                        string input = Console.ReadLine();
+                        int number;
+                        if (int.TryParse(input, out number))
                         {
                             numbers.Add(number);
+                            Console.ForegroundColor = ConsoleColor.White;
                             Console.WriteLine($"{number} added");
                            
                         }
@@ -91,6 +97,7 @@
                             }
 
                             double mean = (double)sum / numbers.Count;
+                            Console.ForegroundColor = ConsoleColor.White;
                             Console.WriteLine($"The mean/average of the numbers is {mean}");
                         }
                         break;
@@ -111,7 +118,7 @@
                                     smallest = num;
                                 }
                             }
-
+                            Console.ForegroundColor = ConsoleColor.White;
                             Console.WriteLine($"The smallest number in the list is {smallest}");
                         }
                         break;
@@ -132,7 +139,7 @@
                                     largest = num;
                                 }
                             }
-
+                            Console.ForegroundColor = ConsoleColor.White;
                             Console.WriteLine($"The largest number in the list is {largest}");
                         }
                         break;
@@ -150,135 +157,128 @@
         }
     }
 }
+        //Console.ForegroundColor = ConsoleColor.Yellow;
+        //Console.WriteLine("\n Menu Select your option:");
+        //Console.WriteLine("=============================");
+        //Console.ForegroundColor = ConsoleColor.Green;
+        //// options of list 
+        //Console.WriteLine("P - Print numbers");
+        //Console.WriteLine("A - Add a number");
+        //Console.WriteLine("M - Display mean of the numbers");
+        //Console.WriteLine("S - Display the smallest number");
+        //Console.WriteLine("L - Display the largest number");
+        //Console.WriteLine("Q - Quit");
+//        Console.WriteLine("Q - Quit");
+//        Console.Write("Enter your choice: ");
+//        //accept uppercase
+//        string input = Console.ReadLine().Trim().ToUpper();
 
-    //static void Main(string[] args)
-    //{
-    //    //List<int> numbers = new List<int>();
-    //    //char choice;
+//        if (input.Length == 0)
+//        {
+//            Console.WriteLine("Unknown selection, please try again!");
+//            continue;
+//        }
 
-    //    List<int> numbers = new List<int>();
-    //    char choice = '\0';
+//        //choice = input[0];
 
-    //    do
-    //    {
-    //        Console.WriteLine("\n Menu:");
-    //        Console.WriteLine("=============================");
-    //        Console.WriteLine("P - Print numbers");
-    //        Console.WriteLine("A - Add a number");
-    //        Console.WriteLine("M - Display mean of the numbers");
-    //        Console.WriteLine("S - Display the smallest number");
-    //        Console.WriteLine("L - Display the largest number");
-    //        Console.WriteLine("Q - Quit");
-    //        Console.Write("Enter your choice: ");
-    //        //accept uppercase
-    //        string input = Console.ReadLine().Trim().ToUpper();
-
-    //        if (input.Length == 0)
-    //        {
-    //            Console.WriteLine("Unknown selection, please try again!");
-    //            continue;
-    //        }
-
-    //        //choice = input[0];
-
-    //        if (choice == 'P')
-    //        {
-    //            if (numbers.Count == 0)
-    //            {
-    //                Console.WriteLine("[] - the list is empty");
-    //            }
-    //            else
-    //            {
-    //                Console.Write("[");
-    //                for (int i = 0; i < numbers.Count; i++)
-    //                {
-    //                    Console.Write(numbers[i]);
-    //                    if (i < numbers.Count - 1)
-    //                    {
-    //                        Console.Write(" ");
-    //                    }
-    //                }
-    //                Console.WriteLine("]");
-    //            }
-    //        }
-    //        else if (choice == 'A')
-    //        {
-    //            Console.Write("Enter an integer to add to the list: ");
-    //            if (int.TryParse(Console.ReadLine(), out int number))
-    //            {
-    //                numbers.Add(number);
-    //                Console.WriteLine($"{number} added");
-    //            }
-    //            else
-    //            {
-    //                Console.WriteLine("Invalid input, Please enter an integer.");
-    //            }
-    //        }
-    //        else if (choice == 'M')
-    //        {
-    //            if (numbers.Count == 0)
-    //            {
-    //                Console.WriteLine("Unable to calculate mean or average--no data");
-    //            }
-    //            else
-    //            {
-    //                int sum = 0;
-    //                foreach (int num in numbers)
-    //                {
-    //                    sum += num;
-    //                }
-    //                double mean = (double)sum / numbers.Count;
-    //                Console.WriteLine($"The mean/averag of the numbers is {mean}");
-    //            }
-    //        }
-    //        else if (choice == 'S')
-    //        {
-    //            if (numbers.Count == 0)
-    //            {
-    //                Console.WriteLine("Unable to determine smallest number in the list - list is empty");
-    //            }
-    //            else
-    //            {
-    //                int smallest = numbers[0];
-    //                foreach (int num in numbers)
-    //                {
-    //                    if (num < smallest)
-    //                    {
-    //                        smallest = num;
-    //                    }
-    //                }
-    //                Console.WriteLine($"The smallest number in the list is {smallest}");
-    //            }
-    //        }
-    //        else if (choice == 'L')
-    //        {
-    //            if (numbers.Count == 0)
-    //            {
-    //                Console.WriteLine("Unable to determine largest number in the list - list is empty");
-    //            }
-    //            else
-    //            {
-    //                int largest = numbers[0];
-    //                foreach (int num in numbers)
-    //                {
-    //                    if (num > largest)
-    //                    {
-    //                        largest = num;
-    //                    }
-    //                }
-    //                Console.WriteLine($"The largest number in the list is {largest}");
-    //            }
-    //        }
-    //        else if (choice == 'Q')
-    //        {
-    //            Console.WriteLine("Goodbye!");
-    //        }
-    //        else
-    //        {
-    //            Console.WriteLine("Unknown Selection, Please try again!");
-    //        }
-    //    } while (choice != 'Q');
-    //}
+//        if (choice == 'P')
+//        {
+//            if (numbers.Count == 0)
+//            {
+//                Console.WriteLine("[] - the list is empty");
+//            }
+//            else
+//            {
+//                Console.Write("[");
+//                for (int i = 0; i < numbers.Count; i++)
+//                {
+//                    Console.Write(numbers[i]);
+//                    if (i < numbers.Count - 1)
+//                    {
+//                        Console.Write(" ");
+//                    }
+//                }
+//                Console.WriteLine("]");
+//            }
+//        }
+//        else if (choice == 'A')
+//        {
+//            Console.Write("Enter an integer to add to the list: ");
+//            if (int.TryParse(Console.ReadLine(), out int number))
+//            {
+//                numbers.Add(number);
+//                Console.WriteLine($"{number} added");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Invalid input, Please enter an integer.");
+//            }
+//        }
+//        else if (choice == 'M')
+//        {
+//            if (numbers.Count == 0)
+//            {
+//                Console.WriteLine("Unable to calculate mean or average--no data");
+//            }
+//            else
+//            {
+//                int sum = 0;
+//                foreach (int num in numbers)
+//                {
+//                    sum += num;
+//                }
+//                double mean = (double)sum / numbers.Count;
+//                Console.WriteLine($"The mean/averag of the numbers is {mean}");
+//            }
+//        }
+//        else if (choice == 'S')
+//        {
+//            if (numbers.Count == 0)
+//            {
+//                Console.WriteLine("Unable to determine smallest number in the list - list is empty");
+//            }
+//            else
+//            {
+//                int smallest = numbers[0];
+//                foreach (int num in numbers)
+//                {
+//                    if (num < smallest)
+//                    {
+//                        smallest = num;
+//                    }
+//                }
+//                Console.WriteLine($"The smallest number in the list is {smallest}");
+//            }
+//        }
+//        else if (choice == 'L')
+//        {
+//            if (numbers.Count == 0)
+//            {
+//                Console.WriteLine("Unable to determine largest number in the list - list is empty");
+//            }
+//            else
+//            {
+//                int largest = numbers[0];
+//                foreach (int num in numbers)
+//                {
+//                    if (num > largest)
+//                    {
+//                        largest = num;
+//                    }
+//                }
+//                Console.WriteLine($"The largest number in the list is {largest}");
+//            }
+//        }
+//        else if (choice == 'Q')
+//        {
+//            Console.WriteLine("Goodbye!");
+//        }
+//        else
+//        {
+//            Console.WriteLine("Unknown Selection, Please try again!");
+//        }
+//    } while (choice != 'Q');
+//}
 
 
 
