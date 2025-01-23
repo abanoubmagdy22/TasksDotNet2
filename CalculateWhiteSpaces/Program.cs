b52c52ac505e444c8076d761bc04c33f5bd562a7
Abanoub Magdy <80099748+abanoubmagdy22@users.noreply.github.com>
2025-01-23 19:11:00 +0200

    day4_methods_codes

---
+++ CalculateWhiteSpaces/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CalculateWhiteSpaces
+{
+    internal class Program
+    {
+
+         static int ClacWhiteSpaces(string value)
+        {
+            int spaceCountOfString = 0;
+            for(int i =0;i < value.Length;i++) 
+            {
+                if (value[i] == ' ')
+                {
+                    spaceCountOfString++;
+                }
+            }
+            return spaceCountOfString; 
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your string:");
+            string x = Console.ReadLine();
+            int numofSpaces = ClacWhiteSpaces(x);
+            Console.WriteLine("the number of spaces is : " + numofSpaces);
+        }
+    }
+}
---
+++ ChallengeDay4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ChallengeDay4
+{
+    internal class Program
+    {
+        static void Calculator(int num1, int num2 ,string op)
+        {
+            switch (op)
+            {
+                case "+":
+                    Console.WriteLine(num1+num2);
+                    break;
+                case "-":
+                    Console.WriteLine(num1 - num2);
+                    break;
+                case "*":
+                    Console.WriteLine(num1 * num2);
+                    break;
+                case "/":
+                    Console.WriteLine(num1 / num2);
+                    break;
+            }
+        }
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            Console.WriteLine("enter first num:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter second num:");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter oprtation");
+            string op = Console.ReadLine();
+            Calculator(num1, num2, op);
+
+        }
+    }
+}
