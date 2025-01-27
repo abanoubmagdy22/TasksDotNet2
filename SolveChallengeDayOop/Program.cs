d7d998cf46c7c38db02284795e34f749af7e7488
Abanoub Magdy <80099748+abanoubmagdy22@users.noreply.github.com>
2025-01-27 19:27:10 +0200

    claass and object oop

---
+++ SolveChallengeDayOop/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace SolveChallengeDayOop
+{
+    class Player
+    {
+        //first require: convert Properties
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public double Experience { get; set; }
+
+        public Player(string name = "Abanoub", int health = 80, double experience = 0.0)
+        {
+            Name = name;
+            Health = health;
+            Experience = experience;
+        }
+
+        //2  Methods
+        public string Shoot()
+        {
+            return $"{Name} is shooting..";
+        }
+
+        public string Trow()
+        {
+            return $"{Name} is throwing..";
+        }
+
+        public string Pass(Player otherPlayer)
+        {
+            return $"{Name} is passing the ball to {otherPlayer.Name}.";
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create players
+            Player player1 = new Player("Pop", 100, 49.5);
+            Player player2 = new Player();
+
+            // Display player names
+            Console.WriteLine(player1.Name);
+            Console.WriteLine(player2.Name);
+
+            // Perform actions
+            Console.WriteLine(player1.Shoot());
+            Console.WriteLine(player1.Trow());
+            Console.WriteLine(player1.Pass(player2));
+        }
+    }
+}
