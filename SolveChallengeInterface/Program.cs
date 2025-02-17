cbd79f7bcee1bd7e96522aa4cd8fdfc862c4be98
Abanoub Magdy <80099748+abanoubmagdy22@users.noreply.github.com>
2025-02-17 10:29:38 +0200

    updste

---
+++ SolveChallengeInterface/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace SolveChallengeInterface
+{
+     interface IPayment
+    {
+        string ProcessPayment(decimal amount);
+    }
+    public class CreditCardPayment : IPayment
+    {
+        public string ProcessPayment(decimal amount)
+        {
+            return " Processes payment with a credit card";
+        }
+    }
+    public class PayPalPayment : IPayment
+    {
+        public string ProcessPayment(decimal amount)
+        {
+            return "Processes payment via PayPal.";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            IPayment CreditCardPayment = new CreditCardPayment();
+            IPayment PayPalPayment = new PayPalPayment();
+            List<IPayment> payments = new List<IPayment>();
+
+            foreach (var payment in payments)
+            {
+                Console.WriteLine(payment.ProcessPayment(100));
+            }
+
+        }
+
+
+    }
+}
